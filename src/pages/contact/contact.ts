feca0e2ad0e5ca79e73b39aa77b7943b99fa597d
Sebastian <sebastian6879@hotmail.com>
2018-11-15 10:42:11 -0500

    conexión de la interfaz inicial con la página introducción ruleta

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {IntroduccionRuletaPage} from "../../pages/introduccion-ruleta/introduccion-ruleta";
 
 @Component({
   selector: 'page-contact',
@@ -10,5 +11,7 @@ export class ContactPage {
   constructor(public navCtrl: NavController) {
 
   }
-
+    irRuletaIntroduccion(){
+        this.navCtrl.push(IntroduccionRuletaPage);
+    }
 }
