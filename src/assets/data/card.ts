4db104f58bc216f061064572c6694b5af37c8d69
benalcazar77 <benalcazar163@hotmail.com>
2019-01-14 23:27:30 -0500

    Interfaces juego de dados - pantalla inicial

---
+++ src/assets/data/card.ts
@@ -0,0 +1,197 @@
+export default
+{
+    "cards": [
+        {
+            "id": "1",
+            "jugador": "Jugador",
+            "reto1": "Tan rapido como puedas",
+            "reto2": "Luego haz 5 abdominales",
+            "accion": "Bebe un shot de tequila"
+        },
+        {
+            "id": "2",
+            "jugador": "Jugador",
+            "reto1": "Tan rapido como puedas",
+            "reto2": "Luego baila la macarena",
+            "accion": "Bebe una cerveza"
+
+        },
+        {
+            "id": "3",
+            "jugador": "Jugador",
+            "reto1": "Dando vueltas",
+            "reto2": "Luego llora drmaticamente",
+            "accion": "Bebe 3 cervezas"
+
+        },
+        {
+            "id": "4",
+            "jugador": "Jugador",
+            "reto1": "Muy lento",
+            "reto2": "Luego camina",
+            "accion": "Bebe 2 shots de vodka"
+
+        },
+        {
+            "id": "5",
+            "jugador": "Jugador",
+            "reto1": "Muy rapido",
+            "reto2": "Luego baila la macarena",
+            "accion": "Bebe 3 cervezas"
+
+        },
+        {
+            "id": "6",
+            "jugador": "Jugador",
+            "reto1": "Tan rapido como puedas",
+            "reto2": "Luego llama a tu ex",
+            "accion": "Bebe 2 shots de vodka"
+        },
+        {
+            "id": "7",
+            "jugador": "Jugador",
+            "reto1": "De forma muy sexy",
+            "reto2": "Luego haz 10 sentadillas",
+            "accion": "Bebe un shot de tequila"
+        },
+        {
+            "id": "8",
+            "jugador": "Jugador",
+            "reto1": "Muy lento",
+            "reto2": "Luego da 8 vueltas",
+            "accion": "Bebe 2 vasos de cerveza"
+
+        },
+        {
+            "id": "9",
+            "jugador": "Jugador",
+            "reto1": "Tan rapido como puedas",
+            "reto2": "Luego besa a quien esta al frente",
+            "accion": "Bebe 1 vso de whisky"
+        },
+        {
+            "id": "10",
+            "jugador": "Jugador",
+            "reto1": "Sentado en el suelo",
+            "reto2": "Luego compra 3 cervezas",
+            "accion": "Bebe una cerveza"
+        },
+        {
+            "id": "10",
+            "jugador": "Jugador",
+            "reto1": "Sentado en el suelo",
+            "reto2": "Luego besa a quien esta al frente",
+            "accion": "Toma un shot de tequila"
+        },
+        {
+            "id": "11",
+            "jugador": "Jugador",
+            "reto1": "Bailando salsa",
+            "reto2": "Luego llama a tu ex",
+            "accion": "Bebe 3 vasos de cerveza"
+        },
+        {
+            "id": "12",
+            "jugador": "Jugador",
+            "reto1": "LLorando",
+            "reto2": "Luego pide el numero a un desconocido",
+            "accion": "Bebe 4 vasos de cerveza"
+        },
+        {
+            "id": "13",
+            "jugador": "Jugador",
+            "reto1": "Tan rapido como puedas",
+            "reto2": "Luego haz 5 abdominales",
+            "accion": "Bebe un shot de tequila"
+        },
+        {
+            "id": "14",
+            "jugador": "Jugador",
+            "reto1": "Tan rapido como puedas",
+            "reto2": "Luego corre",
+            "accion": "Bebe una cerveza"
+
+        },
+        {
+            "id": "15",
+            "jugador": "Jugador",
+            "reto1": "Muy lento",
+            "reto2": "Luego pide el numero a un desconocido",
+            "accion": "Bebe 3 cervezas"
+
+        },
+        {
+            "id": "16",
+            "jugador": "Jugador",
+            "reto1": "Muy lento",
+            "reto2": "Luego haz 5 flexiones",
+            "accion": "Bebe 2 shots de vodka"
+
+        },
+        {
+            "id": "17",
+            "jugador": "Jugador",
+            "reto1": "Muy sensualmente",
+            "reto2": "Luego baila la macarena",
+            "accion": "Bebe 3 cervezas"
+
+        },
+        {
+            "id": "18",
+            "jugador": "Jugador",
+            "reto1": "Tan rapido como puedas",
+            "reto2": "Luego pide el numero a un desconocido",
+            "accion": "Bebe una cerveza"
+        },
+        {
+            "id": "19",
+            "jugador": "Jugador",
+            "reto1": "De forma muy sexy",
+            "reto2": "Luego haz 10 sentadillas",
+            "accion": "Bebe un shot de tequila"
+        },
+        {
+            "id": "20",
+            "jugador": "Jugador",
+            "reto1": "Muy sensualmente",
+            "reto2": "Luego da 8 vueltas",
+            "accion": "Bebe 2 vasos de cerveza"
+
+        },
+        {
+            "id": "21",
+            "jugador": "Jugador",
+            "reto1": "Tan rapido como puedas",
+            "reto2": "Luego haz 5 sentadillas",
+            "accion": "Bebe 1 vso de whisky"
+        },
+        {
+            "id": "22",
+            "jugador": "Jugador",
+            "reto1": "Sentado en el suelo",
+            "reto2": "Luego compra 3 cervezas",
+            "accion": "Bebe una cerveza"
+        },
+        {
+            "id": "23",
+            "jugador": "Jugador",
+            "reto1": "Sentado en el suelo",
+            "reto2": "Luego besa a quien esta al frente",
+            "accion": "Toma un shot de tequila"
+        },
+        {
+            "id": "24",
+            "jugador": "Jugador",
+            "reto1": "Bailando salsa",
+            "reto2": "Luego llama a tu ex",
+            "accion": "Bebe 3 vasos de cerveza"
+        },
+        {
+            "id": "25",
+            "jugador": "Jugador",
+            "reto1": "LLorando",
+            "reto2": "Luego pide el numero a un desconocido",
+            "accion": "Bebe 4 vasos de cerveza"
+        }
+    ]
+};
